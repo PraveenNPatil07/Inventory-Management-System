44df618460698f07f139c0fd2a707544a68d1215
Naveen <amnaveeng@gmail.com>
2025-07-26 09:56:10 +0530

    Product crud sucessfully set up at the backend

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const { createProducts, getProducts } = require('../controllers/product.controller');
+const { createProducts, getProducts, getProductById, updateProduct, deleteProduct } = require('../controllers/product.controller');
 const { protect, authorizeRoles } = require('../utils/verifyUser');
 
 const router = express.Router();
 
+router.post('/createProd', protect, authorizeRoles('admin'), createProducts);
 
-router.post('/createProd',[protect , authorizeRoles],createProducts);
-router.get('/getProducts',getProducts)
+router.get('/getProducts', protect, authorizeRoles('admin', 'staff'), getProducts);
+
+router.get('/getProduct/:id', protect, authorizeRoles('admin', 'staff'), getProductById);
+
+router.put('/updateProd/:id', protect, authorizeRoles('admin'), updateProduct);
+
+router.delete('/deleteProd/:id', protect, authorizeRoles('admin'), deleteProduct);
 
 module.exports = router;
